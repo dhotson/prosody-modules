17196beda6029808f36294a126d9c2547c5329ee
Gaurav <gauravsri@gmail.com>
2010-07-16 01:25:40 -0700

    mod_s2s_reload_newcomponent: fix debug logs

--- mod_s2s_reload_newcomponent/mod_s2s_reload_newcomponent.lua
+++ mod_s2s_reload_newcomponent/mod_s2s_reload_newcomponent.lua
@@ -4,14 +4,11 @@ local config = require "core.configmanager";
 module.host = "*";
 
 local function reload_components()
-        module:log ("debug", "reload_components");
-
         local defined_hosts = config.getconfig();
 
         for host in pairs(defined_hosts) do
-                module:log ("debug", "found host %s", host);
                 if (not hosts[host] and host ~= "*") then
-                        module:log ("debug", "found new host %s", host);
+                        module:log ("debug", "loading new component %s", host);
                         modulemanager.load(host, configmanager.get(host, "core", "component_module"));
                 end
         end;
