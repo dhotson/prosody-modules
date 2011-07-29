c17c2c23c455a7c4d7b82111e44a86c0b1cf9804
Florian Zeitz <florob@babelmonkeys.de>
2011-07-29 22:39:58 +0200

    mod_admin_web: Update dependencies / remove need to build strophe.js (Thanks Zash)

--- mod_admin_web/admin_web/get_deps.sh
+++ mod_admin_web/admin_web/get_deps.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
+JQUERY_VERSION="1.6.2"
+STROPHE_VERSION="1.0.2"
 cd www_files/js
-test -e jquery-1.4.4.min.js || wget http://code.jquery.com/jquery-1.4.4.min.js
+test -e jquery-$JQUERY_VERSION.min.js || wget http://code.jquery.com/jquery-$JQUERY_VERSION.min.js
 test -e adhoc.js || wget http://cgit.babelmonkeys.de/cgit.cgi/adhocweb/plain/js/adhoc.js
-test -e strophe.js || (wget --no-check-certificate -O strophe.tar.gz https://github.com/metajack/strophejs/tarball/release-1.0.1 && \
-	tar xzf strophe.tar.gz && rm strophe.tar.gz && cd metajack-strophejs-*/ && make strophe.js && cp strophe.js ../strophe.js && \
-	cd .. && rm -rf metajack-strophejs*)
-
+test -e strophe.min.js || (wget https://github.com/downloads/metajack/strophejs/strophejs-$STROPHE_VERSION.tar.gz && tar xzf strophejs-$STROPHE_VERSION.tar.gz strophejs-$STROPHE_VERSION/strophe.min.js --strip-components=1 && rm strophejs-$STROPHE_VERSION.tar.gz)
