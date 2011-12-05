c49c064e2ffccf0c163f00911b92ab400689c3e4
Marco Cirillo <maranda@lightwitch.org>
2011-12-05 05:00:28 +0000

    mod_ircd: added channel modes' prefix declaration as some irc clients parse those to detect which mapping to use.

--- mod_ircd/mod_ircd.in.lua
+++ mod_ircd/mod_ircd.in.lua
@@ -257,6 +257,8 @@ local function send_motd(session)
         session.send{from = muc_server, "002", nick, "Your host is "..muc_server.." running Prosody "..prosody.version};
         session.send{from = muc_server, "003", nick, "This server was created the "..os.date(nil, prosody.start_time)}
         session.send{from = muc_server, "004", nick, table.concat({muc_server, "mod_ircd(alpha-0.8)", "i", "aoqv"}, " ")};
+        session.send((":%s %s %s %s :%s"):format(muc_server, "005", nick, "CHANTYPES=# PREFIX=(qaov)~&@+", "are supported by this server"));
+	session.send((":%s %s %s %s :%s"):format(muc_server, "005", nick, "STATUSMSG=~&@+", "are supported by this server"));
         session.send{from = muc_server, "375", nick, "- "..muc_server.." Message of the day -"};
         session.send{from = muc_server, "372", nick, "-"};
         session.send{from = muc_server, "372", nick, "- Please be warned that this is only a partial irc implementation,"};
