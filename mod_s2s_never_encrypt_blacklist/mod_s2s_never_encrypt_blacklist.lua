6e08e6a88d06bfd914f6b2ab2f14717ad5006adb
Marco Cirillo <maranda@lightwitch.org>
2012-01-07 18:12:38 +0000

    mod_s2s_never_encrypt_blacklist: stick to one code "punctuation" style.

--- mod_s2s_never_encrypt_blacklist/mod_s2s_never_encrypt_blacklist.lua
+++ mod_s2s_never_encrypt_blacklist/mod_s2s_never_encrypt_blacklist.lua
@@ -1,16 +1,16 @@
 -- Filter out servers which gets choppy and buggy when it comes to starttls.
 
-local bad_servers = module:get_option_set("tls_s2s_blacklist");
-local bad_servers_ip = module:get_option_set("tls_s2s_blacklist_ip");
+local bad_servers = module:get_option_set("tls_s2s_blacklist")
+local bad_servers_ip = module:get_option_set("tls_s2s_blacklist_ip")
 
 local function disable_tls_for_baddies_in(event)
 	if bad_servers:contains(event.origin.to_host) or bad_servers_ip:contains(event.origin.conn:ip())
-		then event.origin.conn.starttls = nil; end
+		then event.origin.conn.starttls = nil end
 end
 
 local function disable_tls_for_baddies_out(event)
 	if bad_servers:contains(event.origin.from_host) or bad_servers_ip:contains(event.origin.conn:ip())
-		then event.origin.conn.starttls = nil; end
+		then event.origin.conn.starttls = nil end
 end
 
 module:hook("s2s-stream-features", disable_tls_for_baddies_out, 10)
