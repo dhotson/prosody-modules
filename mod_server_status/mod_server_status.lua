49433aed3247e12bef5d4514598bc4698577993d
Marco Cirillo <maranda@lightwitch.org>
2012-01-10 19:36:28 +0000

    mod_server_status: renamed mod_xml_status to mod_server_status as the option to output the stats in JSON was added.

--- mod_server_status/mod_server_status.lua
+++ mod_server_status/mod_server_status.lua
@@ -1,24 +1,26 @@
 -- (C) 2011, Marco Cirillo (LW.Org)
--- Display server stats in readable XML format
+-- Display server stats in readable XML or JSON format
 
 module:set_global()
 
-local ports = module:get_option_array("xml_status_http_ports", {{ port = 5280 }})
-local show_hosts = module:get_option_array("xml_status_show_hosts", nil)
-local show_comps = module:get_option_array("xml_status_show_comps", nil)
+local ports = module:get_option_array("server_status_http_ports", {{ port = 5280 }})
+local show_hosts = module:get_option_array("server_status_show_hosts", nil)
+local show_comps = module:get_option_array("server_status_show_comps", nil)
+local json_output = module:get_option_boolean("server_status_json", false)
 
 local httpserver = require "net.httpserver"
+local json_encode = require "util.json".encode
 
 -- code begin
 
 if not prosody.stanza_counter and not show_hosts and not show_comps then
-	module:log ("error", "mod_xml_status requires at least one of the following things:")
-	module:log ("error", "mod_stanza_counter loaded, or either xml_status_show_hosts or xml_status_show_comps configuration values set.")
-	module:log ("error", "check the module wiki at: http://code.google.com/p/prosody-modules/wiki/mod_xml_status")
+	module:log ("error", "mod_server_status requires at least one of the following things:")
+	module:log ("error", "mod_stanza_counter loaded, or either server_status_show_hosts or server_status_show_comps configuration values set.")
+	module:log ("error", "check the module wiki at: http://code.google.com/p/prosody-modules/wiki/mod_server_status")
 	return false
 end
 
-local r_err = "\n<html>\n<head>\n<title>Prosody's XML Status - Error %s</title>\n<meta name=\"robots\" content=\"noindex, nofollow\" />\n</head>\n\n<body>\n<h3>%s</h3>\n</body>\n\n</html>\n"
+local r_err = "\n<html>\n<head>\n<title>Prosody's Server Status - Error %s</title>\n<meta name=\"robots\" content=\"noindex, nofollow\" />\n</head>\n\n<body>\n<h3>%s</h3>\n</body>\n\n</html>\n"
 
 local response_table = {}
 response_table.header = '<?xml version="1.0" encoding="UTF-8" ?>'
@@ -38,7 +40,7 @@ response_table.comps = {
 		status = '    <status name="%s" current="%s" />'
 }
 
-local function forge_response()
+local function forge_response_xml()
 	local hosts_s = {}; local components = {}; local stats = {}; local hosts_stats = {}; local comps_stats = {}
 
 	local function t_builder(t,r) for _, bstring in ipairs(t) do r[#r+1] = bstring end end
@@ -84,8 +86,27 @@ local function forge_response()
 	result[#result+1] = response_table.header; result[#result+1] = response_table.doc_header -- start
 	t_builder(stats, result); t_builder(hosts_stats, result); t_builder(comps_stats, result)
 	result[#result+1] = response_table.doc_closure -- end
-	
+
 	return table.concat(result, "\n")
+end
+
+local function forge_response_json()
+	local result = {}
+
+	if prosody.stanza_counter then result.stanzas = {} ; result.stanzas = prosody.stanza_counter  end
+	if show_hosts then
+		result.hosts = {}
+		for _,n in ipairs(show_hosts) do result.hosts[n] = hosts[name] and "online" or "offline" end
+	end
+	if show_comps then
+		result.comps = {}
+		for _,n in ipairs(show_hosts) do 
+			result.comps[n] =  hosts[name].modules.component and hosts[name].modules.component.connected and "online" or
+			hosts[name] and hosts[name].modules.component == nil and "online" or "offline"
+		end
+	end
+
+	return json_encode(result)
 end
 
 -- http handlers
@@ -99,7 +120,7 @@ end
 
 local function request(method, body, request)
 	if method == "GET" then
-		return response(200, forge_response())
+		if not json_output then return response(200, forge_response_xml()) else return response(200, forge_response_json()) end
 	else
 		local err405 = r_err:format("405", "Only GET is supported")
 		return response(405, err405, {["Allow"] = "GET"})
@@ -109,7 +130,7 @@ end
 -- initialization.
 -- init http interface
 local function setup()
-	httpserver.new_from_config(ports, request, { base = "xml-status" })
+	httpserver.new_from_config(ports, request, { base = "server-status" })
 end
 
 if prosody.start_time then
