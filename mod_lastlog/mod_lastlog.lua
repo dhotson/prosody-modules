34d129d2722896e3fc3263c6e5bbd1e1cde41fe2
Kim Alvefur <zash@zash.se>
2012-02-24 15:38:14 +0100

    mod_lastlog: Add option to also log the users IP address.

--- mod_lastlog/mod_lastlog.lua
+++ mod_lastlog/mod_lastlog.lua
@@ -1,11 +1,13 @@
 local datamanager = require "util.datamanager";	
 local time = os.time;
+local log_ip = module:get_option_boolean("lastlog_ip_address", false);
 
 module:hook("authentication-success", function(event)
 	local session = event.session;
 	if session.username then
 		datamanager.store(session.username, session.host, "lastlog", {
 			timestamp = time(),
+			ip = log_ip and session.ip or nil,
 		});
 	end
 end);
