1806321f54167449bfd9c52d200d12f1d54a5f6b
Matthew Wild <mwild1@gmail.com>
2012-04-28 19:44:42 +0100

    mod_webpresence: Update to timber, remove squish hack (icons are now loaded relative to the module file)

--- mod_webpresence/mod_webpresence.lua
+++ mod_webpresence/mod_webpresence.lua
@@ -1,49 +1,56 @@
-do local e={};e["icons/status_online.png"]='‰PNG\r\
-\
-\0\0\0\rIHDR\0\0\0\0\0\0\0\0\0ÉV%\0\0\0sBIT|dˆ\0\0\0	pHYs\0\0\r×\0\0\r×B(›x\0\0\0tEXtSoftware\0www.inkscape.org›î<\0\0IDAT(‘U“_h[uÇ?÷woîMÒ65K·el]¡+³Ôa[uõOE\
-"›>DÜ†Pçd0†¸§‚/V‹Ó—¾XAa„Õ)Œá°CtƒiíÃÌ÷ßn7ÓUãÖ$mº&Mn’Ş{×‡;kwàÀáp¾çßs¾Šëº¬\'?êì7C•—jZ­ÛòÙëÔ¢–	TëuÅà—\\;»zVùÜwxGän{f4Û3¿¿×wiIœšƒ/ã“/®ÿæÑ¿Ú^;yÆ^÷½½#’êùûjá‰b³ĞB\0H["—%vÕÆ1ì¬ÍÆkÑ?»RÛ¶<ã\
-€ÙÖ\\b±«Ğ¬*Z@C«{A\rÕPQu%¯3ÎµİjL\0ÛßïŞ•íš{Yh¡T¿úPè y´™ÉŸ\'éï ½éŞ«½¯?û”(‰Òa+dE((BAhÂc3TÍëÕ^Ob0a”­2K­%uÎÍ Uµ-¸àº.¸x"-KÀ«Å¸`ß–}Äzbôíécz÷4Pš­Âª³¢Ò‘È²Ä9îàÔlÓÆ6m¬i‹öÚbğãA.w\\Fnğ/7XQA	GZ’Ğ§!úÍ~ôa»lcm"G"œ9ÅÄù	İ<†ùŒ‰ëx§ULE¨áío.5”"-ã-œ>~š@6@òë$¾><JÓš&âq²³8–ƒ´<æÈõpZı)”[ï<‡H\'\0˜IÏĞë%‘~#t¼gR!X¤´õ÷Ö¾—»™¡Ø½¤ı2Fğİ #\0pèCÜˆİÀ©wp*<øä¦dÄõçüŸ¨3SéÙ/?í˜oYØVÙ\\ávê6X¼¿Èğ•a\
-ÏVÔÌˆş¸îÛÉSCŠëºÄ÷îT&©[ÿôÎ¶ÕB5Â?…1¦2oe@ùßuwƒDÎ®I·º›»\'Î%ó+Æˆïİ©ıQN˜{<ÿJá±‚°TTd˜:\r¿6¸\\\
-}¿©qÃş‰sÉüC®Z±äÓ/VÕêşêÚjg-hEõû¾œÓ¯êø¾ûíÊï_­ı\'i~é&\0\0\0\0IEND®B`‚'e["icons/status_away.png"]='‰PNG\r\
-\
-\0\0\0\rIHDR\0\0\0\0\0\0\0\0\0ÉV%\0\0\0sBIT|dˆ\0\0\0	pHYs\0\0\r×\0\0\r×B(›x\0\0\0tEXtSoftware\0www.inkscape.org›î<\0\01IDAT(‘UÓ_L•u\0Æñïû{ßó¾çQáDš	atÜÌåÔŒp–XºHñg±ş\\°è¦5‡µa«ÉVmı™Ú…EKéE.Bm,\'¹ »HË)Â” àˆ‡Ã9ïyÏû{]Œëçs÷<šRŠ¹9üæã/‡üÙ*¿æ¬ênA<¥Æ¦]ı˜mÛóù¥³s»Ú=¼»nS¸lqâèÓË2Ûü¦Ğ\0”tñ²c3w½³·r¿íI–¼pêÄwï®Û~¶xâry‰¶T˜šn0‹i§‘éiîÄü8œ×.ùpäÔ‰3J\0<šúª¼X-V\0=0}Ş|ô`º?ˆ°Óbø®`Ç7AŠ}±Òm\0âÀ®²m‹ÒÛ5Ã‡0M„Àä sf ÏDjü”K×Å+´ÜXÎšœ‘¯<_ş¨0UzW8à	„@:ša ü~tËfhB§©Ã¡áıf,ËÂÍ¤X»(©[™Ø>c‘y¥@y(Ï)ñ”BIÉéß,Y³“ueëy©z3o¯$èƒ°‘Z&ò,·PII*åĞòC™±‘©$2dğŸIÚ{C¼ñÖ»|ÜÔÈS¡«”†<\0²…"™r¥ç:ìïpˆ.¬¤éd™JI$x§-ÉÁæ6~¾ĞÅà/ÇÙ±QÒ`ÊÖ„1>­EÇbé…}t~xÖ£Í¼wòÒ¨ß{×uù¤ñ5T%ğ%%\0·Ó¾¨³}ùf&òâòöÔ¿ÊGŸ¶Ğª`xèë7<IMÕì«Gw3HÇ@*;0 ‡K"†ˆ×U¥Dtdï/şM}C#6nfoÃël	^`E(—±á¿5¿‘¯º£yõzï_C£‡¾üzÅcyã+WæÛô÷Ñum‚ødœ¡sxî¡ITÖfà`2À×\
-Ú{~½¾_SJQ]³U+²¯÷ÕFFKû3»šGÏ°Å‘gÆ˜YùLzãA>û=4­>ßÙ›=FuÍVCÜh«¼?¶£bÉ”°4CÌ É¬ÉwıóUûÍ§ıáûjÏwvÇş÷ª{Y[¶ªÒ]û@½*×ÌNûnß¼k^–Â×qåRoëÜî¿,3v„ÿ,j”\0\0\0\0IEND®B`‚'e["icons/status_xa.png"]="‰PNG\r\
-\
-\0\0\0\rIHDR\0\0\0\0\0\0\0\0\0ÉV%\0\0\0sBIT|dˆ\0\0\0	pHYs\0\0\r×\0\0\r×B(›x\0\0\0tEXtSoftware\0www.inkscape.org›î<\0\07IDAT(‘U“mLUu\0ÆçÜsÏ=—s¹7¼ˆ£Lo»CØ‚•²jm\r¸ÈIlB«h½¸ÖÚüĞ‹¥sE«¦®üĞzq±”µImI‹,m ®(6³)|P,¯Ø.÷õÜóòïƒéğùüûíùğì‘„,Ï†ğ6¿‘ivFµjYEq”hFñü÷æêûı»å¬tCîmŞ¬ŒE?¯KıÛ¤IHmLÃÁÈÚDm·3¾rÕÑ™PyÇàÀuSîmÚ¬‹^š¬µ—JUMÆ¥È\0X¦CÎp˜OšìKkô¤—-,>_SU180$d€²ØÜ'Ìxé¤¼hº‚îSÈÓTÍÅ>u‘İoÓ_PDİßÿ”¯87s@~µ¡ºiÃÒµÈ/›©ÊM4ïŞÏ¨Ë#ãñºø8«Ó¹k/OtÕıä$XéÊ–‡ëk%•|!àXòPÎKW¤…¦–6ÊÖ…x¶sõbüG£uk;/?µÖØetJâ	—˜‹½%{³F™°÷¶ã};yó•—Xº‡/!Ÿäõ¾÷8BöÄaaÂÿãø’éµÒŠâD­•ğé>7^Ÿ‹Ó¶‡áÂ*>üò+ü~?±XŒÆš{y_“IX¤’&¹¬ÃØíEq9ic›9‡œacdêdƒôÅi¢Ñ(\0Á`®çvp\0†å`›×«Ó’$»jƒ5©T	„€O\r‘7ŞáM\r¼ØÓ…at=ıP%ïşp’»âx€ÉÂ‚Y%®j3ÙŒ]pÚñ ·µÑºµÏ~DŞÏßóÓÉa&FFØ³ÿ\0wÿ‘æúzvÎÇXÔ½3Rä¡ºšû§ş¯L'\\»+ùzâ‹¼¶¹‘ç“×°L‡3¨üZU¦ë”’Î÷Š±ÒÕÍ’‚ÇÃ¡#›ÏN·ge‡ş»Ê‘\\2½§Qm	,K2²L¾æ¡MÉ7§Î^hQ\0ŒĞÚc£æÁÙ+åÛÎ_ ÁõEŒe'(.çi-\\1«–m¿å­íJöÜÌáğÕXÛú…¸¬šÖM1íQä‹‰ÿ±ü;VwŸ>»E¾‘šûÂ*™lwQ:öæÌâEÕ=wUU'…æşvê·?û—³ÿêh°ğj­â\0\0\0\0IEND®B`‚"e["icons/status_dnd.png"]="‰PNG\r\
-\
-\0\0\0\rIHDR\0\0\0\0\0\0\0\0\0ÉV%\0\0\0sBIT|dˆ\0\0\0	pHYs\0\0\r×\0\0\r×B(›x\0\0\0tEXtSoftware\0www.inkscape.org›î<\0\0³IDAT(‘}“Ík\\UÆçÜsîÜ;IBjš\"’–Ñ2hƒĞèRLÀM[pQè¦‹ÿ‚WnDtUQ(ÔnZ*6‚]T„\
-íÂªP-šlL-IÈ$™š™¹wÎÇëâšš øÀwñş^x£D„úğ•Æ;{Êî¬-ËçSïGZ˜å®©ıÔÊëŸôı_ïÜUÛğéÙ£ÃÏ6—?›j¯Íd\
-%!®Œ”E`9Øxû‰}W'¸zyŞ?†OÏZşãî‹áÑxši£ğ.Ò+#EÇÓi:mÏ·{G~;òÜ¡«—çÅ\0h®^xÁµÆk}†¬`m»^D©@‚w‚ë)¦ş|xp­_'şæµ™—7~0XS*¯'ä}†zİ`Óê@ŒBğ‚÷ïï\"v«|æ“_ÎÓŞ:;½ÖÚ¢…1šZ RÙÖZ¡5(­P\
-”‚±Ö_‰¬6Ï™¼(Üé\
-7Ô\0i¡©­+R£1JĞ1A	1Dåi´WéßêL˜>çF—bÂ{s˜Æ9‡sïıfï=Ÿ?ÏÆÜ=7j¶!!Æˆµk-ÿ§è(¥Í¦JVj¥<wö]>Î3´”Bˆ DADˆÖšëœZ©]1­4[œí¹C¯®/‘Ö4Æj’D!@‚ëUY—İ@·ã¨›}ùb²ÿğÓ÷³æ£SCBÇPµªÊ´‚zE ,EØnòwûöÊıá¡÷•ˆp¼1ùÅ÷e:’]Yß•ñ¿ı_«ç\\|jìÚ­{¿¿©ÊÉ‰×'ÆÖ“ô{Õ+‹¸|Ğ_çÊÈğR:6rj×ÇxëØë¦øuñRc¥ùöá–Nuj)·äÎĞëOî?yó›[Í]ğ¶¼ÔxÍt‹“#¢‘÷ÜèfjWWÒô®dö«ŸøåâÎİ¿(çyŞRiû\0\0\0\0IEND®B`‚"e["icons/status_chat.png"]='‰PNG\r\
-\
-\0\0\0\rIHDR\0\0\0\0\0\0\0\0\0ÉV%\0\0\0sBIT|dˆ\0\0\0	pHYs\0\0\r×\0\0\r×B(›x\0\0\0tEXtSoftware\0www.inkscape.org›î<\0\0IDAT(‘UÓLÔuÇñççsßû!ÊáÂİDğ`s¨­qŠY:3©deSË­Øì‡1×ıã¥5d•-ÿ¨›mâfÌrã‡¶6;f-oÓÚZsØˆX6`LÔ»ïîî¾ïş¢àù÷ëñçK‰‹k?·ëP‰Wít:r5c¾0:™¾75m\rLÆŒPó±¿/ŞªüŞñzïã[Œ/¶=‘ÿ¼Óá@4"ÓXb24œÌ~¶ŸûäÄŸo/ÁÇ×{_h°ÿúhõ*¿Rn”r"),I Ö$Ñè3©=]}8²@ÔmâÓ@õJ¿Ò´.Fk/ZûĞº­<€›–-Ínöïo:ğêÆÏ\0Ôç­Ûw¿´//ügµMk6íE)Å²bXÖ}NŸ¾Á†\rï`"‘&ü•¹ô¥o++tyYîÅ•†Ma p\0ËĞº¥\
-PÊEœh´Š††½ôövñìsiêw%]éÙD³a·gkÁBÈY ƒe%,Éä§Nİ¦³óGº{¾buéoÔÔXˆd(-ÍÔ+–gÊE2twP\\<Mı3N”ÌGşÀÉ“g¥7¢ı¬ËÍà-I—33ssãã&×¯—ŸoCé?xzgçÏ±cÇ+¬YSÆË¶ÑÖf ’\0Ò\0˜	ÚLdïD"	jk·sæÌ¾¿bĞŞşß"|“#Gæp»g™,,K1q×yGÏ¦ÔÍƒgëàòåZ[/‹Õ\
-uĞÙÙÁZÿ @\
-ËJ 2À­¡Ü àûu¾Èµ¢”™X%‡WÊÕ«a’ÆÆu’H–‰™ğHÜT7‘¸‰j*ÊjŞˆĞòÑúĞıZ&cyòÚëå‰\\‘={‘¿Ç$nºşCqéê.uë×~-" "ˆœğ÷İşË.ÿD‘}û‹ä§Ÿõ7‘KnÙ²Õ7úäSuŞ%XDx£©ªíâ7ù{lKĞÀ`ß-”ªåá("ÿ¿j¡Í[,Ï›\rVTÌV/se}£cöØÈˆí¦aw^ì¿õåâí¿´b—@0ø3\0\0\0\0IEND®B`‚'e["icons/status_offline.png"]="‰PNG\r\
-\
-\0\0\0\rIHDR\0\0\0\0\0\0\0\0\0ÉV%\0\0\0sBIT|dˆ\0\0\0	pHYs\0\0\r×\0\0\r×B(›x\0\0\0tEXtSoftware\0www.inkscape.org›î<\0\0IDAT(‘U“ÍKcgÅ÷^ïÍÕ\\“˜[;‚º¨SÁAp”)t£m¨L)3V,dp¦ì”`6¡ˆ‚·]ø/BA¡2”JA¦P”®º(ŠÒøµiÚNÒšdD'~Nr?Ş·«XçÀÙ³ù=ÏQ¤”\\×_?V•š„ìAQŞv§ ù»şüâÓåŸ®g•jùÁçC¶iç¢aû¾¦Õ(\0B<ÏÃu].//ÄéÅ«T]>Z~öÜP«EËo5Fo˜f­¨Ú0\0öööÕP0wËrÿ³á{ÊUÙĞßFÂÑÖƒƒ6660Ó41]×ÙÙÙ!•J‘ÉdŞ½<«<P>ß¯F>Íçó˜¦ÉØØ«««H)Ñut:Íøø8###8ƒïû˜F0>0ôñ{5ë­ë†šÉd˜œœdppöövFGG±m›îînâñ8©TŠææft]GJ©—¾Q”›\0±XŒùùy¦¦¦èèè`ee…¦§§YZZbŸÆÆÆÿI£´)ÃœÙÑF«\
-(ŸÏszzÊÜÜ¡Pˆ££#z{{‰ÅbT*Êå2çñêäèDuÇ÷<ïê$MMM‹E\
-…\0¶m“L&ÙŞŞÆó<„\0x®«jm7ßùªÖ¬³¤”ìîî’J¥èëë#™LR©TH$´¶¶²°°€eYhšÆÉi)§úÒË¸®‹ã8d³YºººˆÇãÌÎÎ’Íf™™™abb‚şş~ÙÜÜÄu]|áe”»÷>¼Ğ¬ßÂ¡ˆ¶¾¾ÎÚÚ¥R‰D\"Agg'¾ïS,‘RRWWG©TUÊ“³ãEJÉGw?øŞÜö<\\.‡ªª´µµ¡i¾ï#„àüüß÷©­3yñÏ_?n­§‡T€pÔztx\\øCQ ¥¥Û¶Bàºî Ë²^äşÎEêWï¹üì¹lx«şVáğß¥×åaYÁ7–&„ÏËÃ‚ü3›Y‰D­_~şõèUUuçıÛŸ_<1ŒÀmU©irİÊËrùõ–Z£®¤7÷¾»ı©°g®Á‘‰D\0\0\0\0IEND®B`‚"function require_resource(r)return e[r]or error("resource '"..tostring(r).."' not found");end end local s=require"util.jid".prepped_split;if not require_resource then
-function require_resource(e)local r=io.open((config.get("*","core","presence_icons")or"")..e);if r then
-return r:read("*a");end
-module:log("warn","Failed to open image file %s",(config.get("*","core","presence_icons")or"")..e);return"";end
+module:depends("http");
+
+local jid_split = require "util.jid".prepped_split;
+
+if not require_resource then
+	function require_resource(name)
+		local icon_path = module:get_option_string("presence_icons", "icons");
+		local f, err  = module:load_resource(icon_path.."/"..name);
+		if f then
+			return f:read("*a");
+		end
+		module:log("warn", "Failed to open image file %s", icon_path..name);
+		return "";
+	end
 end
-local e={status="404 Not Found",body="<h1>Page Not Found</h1>Sorry, we couldn't find what you were looking for :("};local n={"online","away","xa","dnd","chat","offline"};for r,e in ipairs(n)do
-n[e]={status="200 OK",headers={["Content-Type"]="image/png"},body=require_resource("icons/status_"..e..".png")};end
-local function t(e,e,o)local e=o.url.path:match("[^/]+$");if e then
-local r,e=s(e);if e and not r then
-r,e=e,o.headers.host;if e then e=e:gsub(":%d+$","");end
+
+local statuses = { "online", "away", "xa", "dnd", "chat", "offline" };
+
+for _, status in ipairs(statuses) do
+	statuses[status] = { status_code = 200, headers = { content_type = "image/png" }, 
+		body = require_resource("status_"..status..".png") };
 end
-if r and e then
-local e=hosts[e]and hosts[e].sessions[r];if e then
-local e=e.top_resources[1];if e and e.presence then
-e=e.presence:child_with_name("show");if not e then
-e="online";else
-e=e:get_text();end
-return n[e];end
+
+local function handle_request(event, path)
+	local jid = path:match("[^/]+$");
+	if jid then
+		local user, host = jid_split(jid);
+		if host and not user then
+			user, host = host, event.request.headers.host;
+			if host then host = host:gsub(":%d+$", ""); end
+		end
+		if user and host then
+			local user_sessions = hosts[host] and hosts[host].sessions[user];
+			if user_sessions then
+				local status = user_sessions.top_resources[1];
+				if status and status.presence then
+					status = status.presence:child_with_name("show");
+					if not status then
+						status = "online";
+					else
+						status = status:get_text();
+					end
+					return statuses[status];
+				end
+			end
+		end
+	end
+	return statuses.offline;
 end
-end
-end
-return n.offline;end
-local e=config.get(module.host,"core","http_ports")or{5280};require"net.httpserver".new_from_config(e,"status",t);
+
+module:provides("http", {
+	default_path = "/status";
+	route = {
+		["GET /*"] = handle_request;
+	};
+});
