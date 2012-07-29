ec3d53164e2193cd5c86fa56b33daf40418bbedf
Kim Alvefur <zash@zash.se>
2012-07-29 21:38:19 +0200

    mod_smacks: Import prosody.core_process_stanza()

--- mod_smacks/mod_smacks.lua
+++ mod_smacks/mod_smacks.lua
@@ -18,6 +18,7 @@ local sm_attr = { xmlns = xmlns_sm };
 local resume_timeout = module:get_option_number("smacks_hibernation_time", 300);
 local s2s_smacks = module:get_option_boolean("smacks_enabled_s2s", false);
 local max_unacked_stanzas = module:get_option_number("smacks_max_unacked_stanzas", 0);
+local core_process_stanza = prosody.core_process_stanza;
 
 local c2s_sessions = module:shared("/*/c2s/sessions");
 local session_registry = {};
