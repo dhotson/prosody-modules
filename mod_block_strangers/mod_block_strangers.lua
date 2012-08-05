b27e349ec79ab763076696b810760db33d0444ac
Matthew Wild <mwild1@gmail.com>
2012-08-05 01:46:24 +0100

    mod_block_strangers: Module to block message and iqs from people not on your roster

---
+++ mod_block_strangers/mod_block_strangers.lua
@@ -0,0 +1,21 @@
+
+local jid_split = require "util.jid".split;
+local jid_bare = require "util.jid".bare;
+local is_contact_subscribed = require "core.rostermanager".is_contact_subscribed;
+
+function check_subscribed(event)
+	local stanza = event.stanza;
+	local to_user, to_host, to_resource = jid_split(stanza.attr.to);
+	local from_jid = jid_bare(stanza.attr.from);
+	if to_user and not is_contact_subscribed(to_user, to_host, from_jid) then
+		if to_resource and stanza.attr.type == "groupchat" then
+			return nil; -- Pass through
+		end
+		return true; -- Drop stanza
+	end
+end
+
+module:hook("message/bare", check_subscribed, 100);
+module:hook("message/full", check_subscribed, 100);
+
+module:hook("iq/full", check_subscribed, 100);
