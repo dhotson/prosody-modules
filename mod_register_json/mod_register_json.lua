27cfbb4d0e93fc73bd51916bbec59d02055c6306
Marco Cirillo <maranda@lightwitch.org>
2013-03-04 14:27:04 +0100

    mod_register_json: correct invalid characters into usernames' log message.

--- mod_register_json/mod_register_json.lua
+++ mod_register_json/mod_register_json.lua
@@ -85,7 +85,7 @@ local function handle_req(event)
 			-- And nodeprep the username
 			local username = nodeprep(req_body["username"])
 			if not username then
-				module:log("debug", "%s supplied an username containing invalid characters: %s", user, username)
+				module:log("debug", "An username containing invalid characters was supplied: %s", user)
 				return http_response(event, 406, "Supplied username contains invalid characters, see RFC 6122.")
 			else
 				if not usermanager.user_exists(username, req_body["host"]) then
