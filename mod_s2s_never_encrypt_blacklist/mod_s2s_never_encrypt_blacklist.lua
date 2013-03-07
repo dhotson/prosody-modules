cf5e3b62ade6f66c265985cd84a8ffc616eec825
Marco Cirillo <maranda@lightwitch.org>
2013-03-07 16:57:17 +0100

    mod_s2s_never_encrypt_blacklist: add missing semicolons.

--- mod_s2s_never_encrypt_blacklist/mod_s2s_never_encrypt_blacklist.lua
+++ mod_s2s_never_encrypt_blacklist/mod_s2s_never_encrypt_blacklist.lua
@@ -7,7 +7,7 @@ local function disable_tls_for_baddies_in(event)
 	local session = event.origin
 	if bad_servers:contains(session.from_host) or bad_servers_ip:contains(session.conn:ip()) then 
 		module:log("debug", "disabling tls on incoming stream from %s...", tostring(session.from_host));
-		session.conn.starttls = nil
+		session.conn.starttls = nil;
 	end
 end
 
@@ -15,7 +15,7 @@ local function disable_tls_for_baddies_out(event)
 	local session = event.origin
 	if bad_servers:contains(session.to_host) then
 		module:log("debug", "disabling tls on outgoing stream from %s...", tostring(session.to_host));
-		session.conn.starttls = nil
+		session.conn.starttls = nil;
 	end
 end
 
