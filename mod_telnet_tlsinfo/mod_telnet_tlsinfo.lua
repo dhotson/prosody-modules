b5c67f5f4425675fe3676fb7389c8acf809802d3
Kim Alvefur <zash@zash.se>
2013-06-27 18:22:53 +0200

    mod_telnet_tlsinfo: Initial commit. Shows ciphers used and other TLS info

---
+++ mod_telnet_tlsinfo/mod_telnet_tlsinfo.lua
@@ -0,0 +1,41 @@
+-- mod_telnet_tlsinfo.lua
+
+module:set_global();
+module:depends("admin_telnet");
+
+local console_env = module:shared("/*/admin_telnet/env");
+local c2s_sessions = module:shared("/*/c2s/sessions");
+local s2s_sessions = module:shared("/*/s2s/sessions");
+
+local function print_tlsinfo(print, session)
+	if session.secure then
+		local sock = session.conn:socket()
+		for k,v in pairs(sock:info()) do
+			print(("%20s: %s"):format(k, tostring(v)))
+		end
+	else
+		print(("%20s: %s"):format("protocol", "TCP"))
+	end
+end
+
+function console_env.c2s:showtls()
+	local print = self.session.print;
+	for _, session in pairs(c2s_sessions) do
+		print(session.full_jid or "unauthenticated")
+		print_tlsinfo(print, session);
+		print""
+	end
+end
+
+function console_env.s2s:showtls()
+	local print = self.session.print;
+	for _, session in pairs(s2s_sessions) do
+		if session.direction == "outgoing" then
+			print(session.from_host, "->", session.to_host)
+		else
+			print(session.to_host, "<-", session.from_host)
+		end
+		print_tlsinfo(print, session);
+		print""
+	end
+end
