f7b6b03d16be79ee8a816bc3c1deec4799a14cc9
Kim Alvefur <zash@zash.se>
2013-06-30 01:13:49 +0200

    mod_log_auth: Don't cause an error if any of the logged fields are missing

--- mod_log_auth/mod_log_auth.lua
+++ mod_log_auth/mod_log_auth.lua
@@ -1,4 +1,4 @@
 
 module:hook("authentication-failure", function (event)
-	module:log("info", "Failed authentication attempt (%s) from IP: %s", event.condition, event.session.ip);
+	module:log("info", "Failed authentication attempt (%s) from IP: %s", event.condition or "unknown-condition", event.session.ip or "?");
 end);
