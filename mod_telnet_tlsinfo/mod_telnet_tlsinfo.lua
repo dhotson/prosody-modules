cb41ad36b91b49aa6417916a13fd3a8fedd10293
Kim Alvefur <zash@zash.se>
2013-08-07 11:13:54 +0200

    mod_telnet_tlsinfo: Add a pattern argument similar to s2s:show(pattern) for limiting output

--- mod_telnet_tlsinfo/mod_telnet_tlsinfo.lua
+++ mod_telnet_tlsinfo/mod_telnet_tlsinfo.lua
@@ -18,24 +18,28 @@ local function print_tlsinfo(print, session)
 	end
 end
 
-function console_env.c2s:showtls()
+function console_env.c2s:showtls(pat)
 	local print = self.session.print;
 	for _, session in pairs(c2s_sessions) do
-		print(session.full_jid or "unauthenticated")
-		print_tlsinfo(print, session);
-		print""
+		if not pat or session.full_jid and session.full_jid:find(pat, nil, true) then
+			print(session.full_jid or "unauthenticated")
+			print_tlsinfo(print, session);
+			print""
+		end
 	end
 end
 
-function console_env.s2s:showtls()
+function console_env.s2s:showtls(pat)
 	local print = self.session.print;
 	for _, session in pairs(s2s_sessions) do
-		if session.direction == "outgoing" then
-			print(session.from_host, "->", session.to_host)
-		else
-			print(session.to_host, "<-", session.from_host)
+		if not pat or session.from_host == pat or session.to_host == pat then
+			if session.direction == "outgoing" then
+				print(session.from_host, "->", session.to_host)
+			else
+				print(session.to_host, "<-", session.from_host)
+			end
+			print_tlsinfo(print, session);
+			print""
 		end
-		print_tlsinfo(print, session);
-		print""
 	end
 end
