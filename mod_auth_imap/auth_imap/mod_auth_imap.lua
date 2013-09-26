3829a1ce7546258974a99f91ef3f228ccf239d59
Matthew Wild <mwild1@gmail.com>
2013-09-26 18:11:44 +0100

    mod_auth_imap: Leave port nil if not specified in the config, so we can auto-detect based on whether we use SSL

--- mod_auth_imap/auth_imap/mod_auth_imap.lua
+++ mod_auth_imap/auth_imap/mod_auth_imap.lua
@@ -6,7 +6,7 @@ local name = "IMAP SASL";
 local log = require "util.logger".init("auth_imap");
 
 local imap_host = module:get_option_string("imap_auth_host", "localhost");
-local imap_port = module:get_option_number("imap_auth_port", 143);
+local imap_port = module:get_option_number("imap_auth_port");
 
 
 local imap_service_realm = module:get_option("imap_service_realm");
