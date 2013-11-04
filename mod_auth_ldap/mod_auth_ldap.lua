0ed51fbad791fbf43663997f18c33e7300af4d14
Kim Alvefur <zash@zash.se>
2013-11-04 18:13:57 +0100

    mod_auth_ldap: Fix set_password

--- mod_auth_ldap/mod_auth_ldap.lua
+++ mod_auth_ldap/mod_auth_ldap.lua
@@ -43,10 +43,8 @@ end
 function provider.set_password(username, password)
 	local dn, attr = get_user(username);
 	if not dn then return nil, attr end
-	if attr.password ~= password then
-		ld:modify(dn, { '=', userPassword = password });
-	end
-	return true
+	if attr.userPassword == password then return true end
+	return ld:modify(dn, { '=', userPassword = password })();
 end
 function provider.create_user(username, password) return nil, "Account creation not available with LDAP."; end
 
