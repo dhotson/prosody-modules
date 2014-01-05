b8053ac3897ca0103622a5d4c5de8e6141d066b9
Florian Zeitz <florob@babelmonkeys.de>
2014-01-05 01:39:49 +0100

    mod_admin_web: Bump strophe.js version

--- mod_admin_web/admin_web/get_deps.sh
+++ mod_admin_web/admin_web/get_deps.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 JQUERY_VERSION="1.10.2"
-STROPHE_VERSION="1.1.1"
+STROPHE_VERSION="1.1.2"
 BOOTSTRAP_VERSION="1.4.0"
 ADHOC_COMMITISH="87bfedccdb91e2ff7cfb165e989e5259c155b513"
 
 cd www_files/js
 
 rm -f jquery-$JQUERY_VERSION.min.js
-wget http://code.jquery.com/jquery-$JQUERY_VERSION.min.js
+wget http://code.jquery.com/jquery-$JQUERY_VERSION.min.js || exit 1
 
 rm -f adhoc.js
-wget -O adhoc.js "http://git.babelmonkeys.de/?p=adhocweb.git;a=blob_plain;f=js/adhoc.js;hb=$ADHOC_COMMITISH"
+wget -O adhoc.js "http://git.babelmonkeys.de/?p=adhocweb.git;a=blob_plain;f=js/adhoc.js;hb=$ADHOC_COMMITISH" || exit 1
 
 rm -f strophe.min.js
-wget https://raw.github.com/strophe/strophe.im/gh-pages/strophejs/downloads/strophejs-$STROPHE_VERSION.tar.gz && tar xzf strophejs-$STROPHE_VERSION.tar.gz strophejs-$STROPHE_VERSION/strophe.min.js --strip-components=1 && rm strophejs-$STROPHE_VERSION.tar.gz
+wget https://raw.github.com/strophe/strophe.im/gh-pages/strophejs/downloads/strophejs-$STROPHE_VERSION.tar.gz && tar xzf strophejs-$STROPHE_VERSION.tar.gz strophejs-$STROPHE_VERSION/strophe.min.js --strip-components=1 && rm strophejs-$STROPHE_VERSION.tar.gz || exit 1
 
 cd ../css
 rm -f bootstrap-$BOOTSTRAP_VERSION.min.css
-wget https://raw.github.com/twbs/bootstrap/v$BOOTSTRAP_VERSION/bootstrap.min.css -O bootstrap-$BOOTSTRAP_VERSION.min.css
+wget https://raw.github.com/twbs/bootstrap/v$BOOTSTRAP_VERSION/bootstrap.min.css -O bootstrap-$BOOTSTRAP_VERSION.min.css || exit 1
