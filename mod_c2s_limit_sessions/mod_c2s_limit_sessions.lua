879f36573afbe67e03479ff364ab58d283b5fd2d
Kim Alvefur <zash@zash.se>
2014-03-19 23:17:55 +0100

    mod_c2s_limit_sessions: Fix global access

--- mod_c2s_limit_sessions/mod_c2s_limit_sessions.lua
+++ mod_c2s_limit_sessions/mod_c2s_limit_sessions.lua
@@ -6,7 +6,8 @@ local max_resources = module:get_option_number("max_resources", 10);
 
 local sessions = hosts[module.host].sessions;
 module:hook("resource-bind", function(event)
-	if count(next, sessions[event.session.username].sessions) > max_resources then
+	local session = event.session;
+	if count(next, sessions[session.username].sessions) > max_resources then
 		session:close{ condition = "policy-violation", text = "Too many resources" };
 		return false
 	end
