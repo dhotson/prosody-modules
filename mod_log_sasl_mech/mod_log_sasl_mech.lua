99579ef0706ef32f365703fbcedfc8958c4069cf
Kim Alvefur <zash@zash.se>
2014-04-25 17:11:33 +0200

    mod_log_sasl_mech: Log SASL mechanism attached to session

--- mod_log_sasl_mech/mod_log_sasl_mech.lua
+++ mod_log_sasl_mech/mod_log_sasl_mech.lua
@@ -1,5 +1,6 @@
 
 module:hook("authentication-success", function (event)
-	local sasl_handler = event.session.sasl_handler;
-	module:log("info", "Authenticated with %s", sasl_handler and sasl_handler.selected or "legacy auth");
+	local session = event.session;
+	local sasl_handler = session.sasl_handler;
+	session.log("info", "Authenticated with %s", sasl_handler and sasl_handler.selected or "legacy auth");
 end);
