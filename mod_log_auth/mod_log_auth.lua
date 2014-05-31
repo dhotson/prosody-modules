81313e82f820da5b49f4fe5aa0071ad49beac076
Matthew Wild <mwild1@gmail.com>
2014-05-31 13:38:35 +0100

    mod_log_auth: Add ability to log IPs of successful authentications too

--- mod_log_auth/mod_log_auth.lua
+++ mod_log_auth/mod_log_auth.lua
@@ -1,4 +1,15 @@
+local mode = module:get_option_string("log_auth_ips", "failure");
+assert(({ all = true, failure = true, success = true })[mode], "Unknown log mode: "..tostring(mode).." - valid modes are 'all', 'failure', 'success'");
 
-module:hook("authentication-failure", function (event)
-	module:log("info", "Failed authentication attempt (%s) from IP: %s", event.condition or "unknown-condition", event.session.ip or "?");
-end);
+if mode == "failure" or mode == "all" then
+	module:hook("authentication-failure", function (event)
+		module:log("info", "Failed authentication attempt (%s) from IP: %s", event.condition or "unknown-condition", event.session.ip or "?");
+	end);
+end
+
+if mode == "success" or mode == "all" then
+	module:hook("authentication-success", function (event)
+		local session = event.session;
+		module:log("info", "Successful authentication as %s from IP: %s", session.username, session.ip or "?");
+	end);
+end
