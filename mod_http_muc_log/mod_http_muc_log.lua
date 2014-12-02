5dfe4f8b110195c3cf0e3423acc34d4bcaad35b0
Kim Alvefur <zash@zash.se>
2014-12-02 14:17:08 +0100

    mod_http_muc_log: List rooms with logging explicitly enabled

--- mod_http_muc_log/mod_http_muc_log.lua
+++ mod_http_muc_log/mod_http_muc_log.lua
@@ -206,7 +206,7 @@ local function public_room(room)
 	return (room
 		and not (room.get_hidden or room.is_hidden)(room)
 		and not (room.get_members_only or room.is_members_only)(room)
-		and room._data.logging ~= false);
+		and room._data.logging == true);
 end
 
 -- FIXME Invent some more efficient API for this
