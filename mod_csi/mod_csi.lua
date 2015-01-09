fc62ba1e8825e5a5e201446519d2bfddccaaef59
Kim Alvefur <zash@zash.se>
2015-01-09 11:36:31 +0100

    mod_csi: Update namespace to published version

--- mod_csi/mod_csi.lua
+++ mod_csi/mod_csi.lua
@@ -1,5 +1,6 @@
 local st = require "util.stanza";
-local csi_feature = st.stanza("csi", { xmlns = "urn:xmpp:csi" });
+local xmlns_csi = "urn:xmpp:csi:0";
+local csi_feature = st.stanza("csi", { xmlns = xmlns_csi });
 
 module:hook("stream-features", function (event)
 	if event.origin.username then
@@ -14,9 +15,8 @@ function refire_event(name)
 			return true;
 		end
 	end;
-		
 end
 
-module:hook("stanza/urn:xmpp:csi:active", refire_event("csi-client-active"));
-module:hook("stanza/urn:xmpp:csi:inactive", refire_event("csi-client-inactive"));
+module:hook("stanza/"..xmlns_csi..":active", refire_event("csi-client-active"));
+module:hook("stanza/"..xmlns_csi..":inactive", refire_event("csi-client-inactive"));
 
